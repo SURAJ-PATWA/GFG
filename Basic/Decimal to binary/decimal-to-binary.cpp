3841d2473958ad7d6d6c49216424651033c34239
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-04-23 21:45:56 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Decimal to binary/decimal-to-binary.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+void toBinary(int n)
+{
+    // your code here
+    
+    if(n==0)return ;
+    
+    toBinary(n/2);
+    cout<<n%2;
+    
+        
+}
+
+//{ Driver Code Starts.
+
+int main() {
+	//code
+	
+	int t;
+	cin>>t;
+	
+	
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    toBinary(n);
+	    cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
