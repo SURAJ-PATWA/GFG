88c675a2a22908207ddd3795b1910afa3c910a9a
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-05-01 22:19:21 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Common Divisors/common-divisors.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    long long int commDiv(long long int a,long long int b) {
+        // code here
+        long long count=0;
+        for(int i=1;i<=min(a,b);i++){
+            if(a%i==0 && b%i==0){
+                count++;
+            }
+        }
+        return count;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long int a, b;
+        cin >> a >> b;
+        Solution ob;
+        cout<<ob.commDiv(a, b)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
