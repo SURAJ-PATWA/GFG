c87312c51549fe1f79db9c068075930aa5243c51
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-06-17 09:17:29 +0530

    Updated Solution - GfG to GitHub

--- Easy/Subarray with given sum/subarray-with-given-sum.cpp
+++ Easy/Subarray with given sum/subarray-with-given-sum.cpp
@@ -17,13 +17,11 @@ class Solution
     if(sum==0)return {-1};
     
     while (right <= n) {
-        // If current sum exceeds the target sum, remove elements from the left
         while (curr_sum > sum && left < right) {
             curr_sum -= arr[left];
             left++;
         }
         
-        // If current sum equals the target sum, return the subarray indices
         if (curr_sum == sum) {
             vector<int> result;
             result.push_back(left + 1);  // Convert to 1-based indexing
@@ -31,17 +29,14 @@ class Solution
             return result;
         }
         
-        // If current sum is less than the target sum, add the next element to the current sum
-        if (right < n)
+        if (right < n){
             curr_sum += arr[right];
-        
+        }
         right++;
     }
     
-    // If no subarray found, return {-1}
-    vector<int> result;
-    result.push_back(-1);
-    return result;
+
+    return {-1};
 
     }
 };
