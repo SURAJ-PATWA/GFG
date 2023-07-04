c398d25f845caf9fc876c533729606141e469765
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-07-04 20:16:24 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count pairs with given sum/count-pairs-with-given-sum.cpp
+++ Easy/Count pairs with given sum/count-pairs-with-given-sum.cpp
@@ -11,17 +11,16 @@ class Solution{
 public:
     int getPairsCount(int arr[], int n, int sum) {
         // code here
-        unordered_map<int,int>mp;
         int ans=0;
+        unordered_map<int,int>mp;
         for(int i=0;i<n;i++){
-        int b=sum-arr[i];
-         if(mp[b]){
-            ans+=mp[b];
-         }
-         mp[arr[i]]++;
+            int b=sum-arr[i];
+            if(mp[b]){
+                ans+=mp[b];
+            }
+            mp[arr[i]]++;
         }
         return ans;
-                                                                           
     }
 };
 
