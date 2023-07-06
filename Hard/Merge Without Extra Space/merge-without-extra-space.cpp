88daf9af1241cd8f8c1ad38fdb26902ed4045b05
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-07-07 03:15:55 +0530

    Updated Solution - GfG to GitHub

--- Hard/Merge Without Extra Space/merge-without-extra-space.cpp
+++ Hard/Merge Without Extra Space/merge-without-extra-space.cpp
@@ -10,36 +10,20 @@ class Solution{
         void merge(long long arr1[], long long arr2[], int n, int m) 
         { 
             // code here 
-            long long arr[n+m];
-            for(int i=0;i<n;i++){
-                arr[i]=arr1[i];
+            int i=0;int j=0;int k=n-1;
+            while(i<=k && j<m){
+                if(arr1[i]<=arr2[j]){
+                    i++;
+                }
+                else{
+                    swap(arr2[j++],arr1[k--]);
+                }
             }
-            for(int j=0;j<m;j++){
-                arr[n+j]=arr2[j];
-            }
-            sort(arr,arr+n+m);
-            for(int i=0;i<n;i++){
-                arr1[i]=arr[i];
-            }
-            for(int i=0;i<m;i++){
-                arr2[i]=arr[n+i];
-            }
+            sort(arr1,arr1+n);
+            sort(arr2,arr2+m);
+            
             
         } 
-    //     long arr[]=new long[m+n];
-    // for(int i=0;i<n;i++){
-    //     arr[i]=arr1[i];
-    // }
-    // for(int j=0;j<m;j++){
-    //     arr[n+j]=arr2[j];
-    // }
-    // Arrays.sort(arr);
-    // for(int i=0;i<n;i++){
-    //     arr1[i]=arr[i];
-    // }
-    //  for(int j=0;j<m;j++){
-    //      arr2[j]=arr[n+j];
-    //  }
 };
 
 //{ Driver Code Starts.
