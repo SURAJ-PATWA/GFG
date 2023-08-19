240f066780ee87d911b1cebd112c9f92ae045715
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-08-19 22:51:13 +0530

    Updated Solution - GfG to GitHub

--- Easy/Subarray with given sum/subarray-with-given-sum.cpp
+++ Easy/Subarray with given sum/subarray-with-given-sum.cpp
@@ -10,57 +10,22 @@ class Solution
     //Function to find a continuous sub-array which adds up to a given number.
     vector<int> subarraySum(vector<int>arr, int n, long long sum)
     {
-        // Your code here 
-        // i will try to solve window sum
-        int curr_sum=arr[0] ;
-        int start=0;
-        for(int i=1;i<=n;i++){
-            while(curr_sum>sum && start<i-1){
-               curr_sum-=arr[start];
-               start++;
-            }
-            if(curr_sum==sum){
-              return {start+1,i};  
-            }
-            //if(i<n){
-             curr_sum+=arr[i];   
-            //}
-        }
+        // Your code here
+        int curr_sum=arr[0];
+   int start=0;
+   
+   for(int i=1;i<=n;i++){
+       while(curr_sum>sum && start<i-1){
+           curr_sum-=arr[start];start++;
+       }
+       if(curr_sum==sum){
+           return {start+1,i};
+       }
+       curr_sum+=arr[i];
+   }
+   
         
-        
-        return {-1};
-        
-        
-        
-        
-        
-        
-    //     int left = 0, right = 0;
-    // int curr_sum = 0;
-    // if(sum==0)return {-1};
-    
-    // while (right <= n) {
-    //     while (curr_sum > sum && left < right) {
-    //         curr_sum -= arr[left];
-    //         left++;
-    //     }
-        
-    //     if (curr_sum == sum) {
-    //         vector<int> result;
-    //         result.push_back(left + 1);  // Convert to 1-based indexing
-    //         result.push_back(right);
-    //         return result;
-    //     }
-        
-    //     if (right < n){
-    //         curr_sum += arr[right];
-    //     }
-    //     right++;
-    // }
-    
-
-    // return {-1};
-
+       return {-1}; 
     }
 };
 
