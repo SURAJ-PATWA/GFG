b93bf6ac41fb4498095d92f9253ae9695b471111
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-10-30 11:41:51 +0530

    Updated Solution - GfG to GitHub

--- Basic/Find minimum and maximum element in an array/find-minimum-and-maximum-element-in-an-array.cpp
+++ Basic/Find minimum and maximum element in an array/find-minimum-and-maximum-element-in-an-array.cpp
@@ -24,9 +24,21 @@ int main() {
 
 
 pair<long long, long long> getMinMax(long long a[], int n) {
-   sort(a,a+n);
-   
-   return {a[0],a[n-1]};
-   
+    int maxi=INT_MIN;
+    int mini=INT_MAX;
+    
+    for(int i=0;i<n;i++){
+        if(a[i]>maxi){
+            maxi=a[i];
+        }
+    }
+    
+    for(int i=0;i<n;i++){
+        if(a[i]<mini){
+            mini=a[i];
+        }
+    }
+    
+    return {mini,maxi};
     
 }
