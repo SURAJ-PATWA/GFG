58ecd83574667a162baa9b6d3107537ebb8c663f
SURAJ PATWA <93833744+SURAJ-PATWA@users.noreply.github.com>
2023-11-04 23:05:03 +0530

    Updated Solution - GfG to GitHub

--- Easy/Min Subsets with Consecutive Numbers/min-subsets-with-consecutive-numbers.cpp
+++ Easy/Min Subsets with Consecutive Numbers/min-subsets-with-consecutive-numbers.cpp
@@ -8,15 +8,14 @@ class Solution{
     int numofsubset(int arr[], int n)
     {
         // Your code goes here
-          sort(arr,arr+n);
-        int count=0;
+        sort(arr,arr+n);
+        int count=1;
         for(int i=1;i<n;i++){
-            
             if(arr[i]-arr[i-1]!=1){
                 count++;
             }
         }
-        return count+1;
+        return count;
     }
 };
 
